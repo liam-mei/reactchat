b17d2ba5b3bd9aaf9cf931688df3213d0fe711a6
Derrick Mei <derrick.mei.168@gmail.com>
2020-08-18 01:34:49 -0500

    added react-router-dom

--- reactchat/src/socket/socketConnection.js
+++ reactchat/src/socket/socketConnection.js
@@ -4,4 +4,9 @@ let socket = io.connect("http://localhost:5000");
 
 socket.emit("clientTopSocketTest", "I work");
 
+// socket.on("rooms", (rooms) => {
+//   console.log("client socket received rooms");
+//   // setRooms(rooms);
+// });
+
 export default socket;
