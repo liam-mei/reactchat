d6c5dbdd46bf97d9e550c683c333129c0ab1d21a
Derrick Mei <derrick.mei.168@gmail.com>
2020-08-26 02:09:44 -0500

    updated App.js with navbar and approuter

--- reactchat/src/App.js
+++ reactchat/src/App.js
@@ -1,24 +1,15 @@
 import React, { useEffect, useState } from "react";
+import Navbar from "./components/Navbar";
+import AppRouter from "./components/AppRouter";
 import "./App.css";
 
 import socket from "./socket/socketConnection";
 
 function App() {
-  const [rooms, setRooms] = useState([""]);
-
-  useEffect(() => {
-    socket.on("rooms", (rooms) => {
-      console.log("client socket received rooms");
-      setRooms(rooms);
-    });
-  }, []);
   return (
-    <div>
-      <div>
-        {rooms.map((room) => (
-          <div>{room}</div>
-        ))}
-      </div>
+    <div className='app'>
+      <Navbar />
+      <AppRouter />
     </div>
   );
 }
