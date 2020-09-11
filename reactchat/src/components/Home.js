5ccf6c3be3e208a3093aade0be6713cbed1a39b7
Derrick Mei <derrick.mei.168@gmail.com>
2020-09-11 03:48:16 -0500

    updated home styling and composition

--- reactchat/src/components/Home.js
+++ reactchat/src/components/Home.js
@@ -1,11 +1,14 @@
 import React from "react";
+import { Link } from "react-router-dom";
+
 import Login from "./Login";
+import HomeNav from "./HomeNav";
 
 export default function Home() {
   return (
-    <div>
-      Home Component
+    <>
+      <HomeNav />
       <Login />
-    </div>
+    </>
   );
 }
