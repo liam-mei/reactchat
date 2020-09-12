660591d6cc3a721ff859c0b5c2081b9259732bc0
Derrick Mei <derrick.mei.168@gmail.com>
2020-09-12 03:50:09 -0500

    updated navbar with styling and settings logo

--- reactchat/src/components/Navbar.js
+++ reactchat/src/components/Navbar.js
@@ -1,53 +1,39 @@
 import React from "react";
-import { NavLink } from "react-router-dom";
+import { useHistory } from "react-router-dom";
+
 import Navbar from "react-bootstrap/Navbar";
 import NavDropdown from "react-bootstrap/NavDropdown";
-import Nav from "react-bootstrap/Nav";
 import settingsIcon from "../pictures/settings.png";
+import darkFavicon from "../pictures/faviconDark.ico";
 
 export default function Navbar2() {
+  const history = useHistory();
   const removeToken = () => {
     localStorage.removeItem("token");
+    history.push("/");
   };
 
   return (
     <>
-      <div>I'm not sticky!</div>
-      <Navbar bg="light" expand="lg" sticky="top">
-        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
-        {/* <Navbar.Toggle aria-controls="basic-navbar-nav" /> */}
-        {/* <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="mr-auto">
-            <NavLink className="navbar" to="/">
-              Home
-            </NavLink>
-            <NavLink className="navbar" to="/login">
-              Login
-            </NavLink>
-            <NavLink className="navbar" to="/register">
-              Register
-            </NavLink>
-            <NavLink className="navbar" to="/rooms">
-              Find Chat Room
-            </NavLink>>
-          </Nav>
-          <div>I'm sticky!!!</div>
-        </Navbar.Collapse> */}
-        <NavDropdown title={<img className='icon' src={settingsIcon}/>} id="basic-nav-dropdown">
-          <NavDropdown.Item href="#action/3.1">Settings</NavDropdown.Item>
+      <Navbar bg="light" expand="lg" sticky="top" className='d-flex justify-content-start'>
+        <Navbar.Brand className="d-flex mr-auto">
+          <img className="logo" src={darkFavicon} />
+          <div className="m-2 pl-3">Message Me</div>
+        </Navbar.Brand>
+        <NavDropdown
+          title={<img className="icon" src={settingsIcon} />}
+          id="basic-nav-dropdown"
+          className=''
+        >
+          <NavDropdown.Item>Settings</NavDropdown.Item>
           <NavDropdown.Divider />
-          <NavDropdown.Item href="#action/3.2">About</NavDropdown.Item>
-          <NavDropdown.Item href="#action/3.3">Terms</NavDropdown.Item>
-          <NavDropdown.Item href="#action/3.3">Privacy Policy</NavDropdown.Item>
-          <NavDropdown.Item href="#action/3.3">Cookie Policy</NavDropdown.Item>
+          <NavDropdown.Item>About</NavDropdown.Item>
+          <NavDropdown.Item>Terms</NavDropdown.Item>
+          <NavDropdown.Item>Privacy Policy</NavDropdown.Item>
+          <NavDropdown.Item>Cookie Policy</NavDropdown.Item>
           <NavDropdown.Divider />
-          <NavDropdown.Item href="#action/3.4" onClick={removeToken}>
-            Log Out
-          </NavDropdown.Item>
+          <NavDropdown.Item onClick={removeToken}>Log Out</NavDropdown.Item>
         </NavDropdown>
-        {/* <NavLink className="navbar" onClick={removeToken} to="/">
-          Log Out
-        </NavLink> */}
       </Navbar>
     </>
   );
