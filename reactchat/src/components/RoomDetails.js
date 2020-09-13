1c4b44addce014d8e3a33d54027896d424bfb10d
Derrick Mei <derrick.mei.168@gmail.com>
2020-09-13 11:44:35 -0500

    created roomdetails component

---
+++ reactchat/src/components/RoomDetails.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function RoomDetails() {
+  return (
+    <div className='details d-flex'>
+      <div className='innerDetails'>Room Details</div>
+    </div>
+  )
+}
