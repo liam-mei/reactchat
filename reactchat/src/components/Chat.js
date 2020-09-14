e864ab772e9653249a50d7fb038b2bf56bffdf9c
Derrick Mei <derrick.mei.168@gmail.com>
2020-09-13 23:05:36 -0500

    added callback to useEffect to  cleanup socket connection

--- reactchat/src/components/Chat.js
+++ reactchat/src/components/Chat.js
@@ -7,37 +7,68 @@ import Rooms from "./Rooms";
 import Room from "./Room";
 
 export default function Chat(props) {
+  const username = localStorage.getItem("username");
   const history = useHistory();
-  const [state, setState] = useState({
-    rooms: [
-      { id: 1, name: "fakeRoom1" },
-      { id: 2, name: "fakeRoom2" },
-      { id: 3, name: "fakeRoom3" },
-    ],
-    currentRoom: {},
-  });
+  const [rooms, setRooms] = useState([
+    { id: 1, name: "fakeRoom1" },
+    { id: 2, name: "fakeRoom2" },
+    { id: 3, name: "fakeRoom3" },
+  ]);
+  const [currentRoom, setCurrentRoom] = useState({});
+  const [messages, setMessages] = useState([
+    { message: "fakeMessage1", User: { username: "fakeuser1" } },
+  ]);
 
   useEffect(() => {
     socket.emit("getRooms");
 
     socket.on("rooms", (rooms) => {
-      console.log(rooms)
-      setState({ rooms, currentRoom: rooms[0] });
+      console.log(rooms);
+      setRooms(rooms);
+      setCurrentRoom(rooms[0]);
       history.push(`/rooms/${rooms[0].id}`);
     });
-    
 
-  }, []);
+    socket.on("newMessage", (message) => {
+      console.log({ newMessage: message });
+      console.log({currentMessages: messages.slice()})
+      setMessages([...messages, message]);
+    });
+
+    socket.on("currentMessages", (currentMessages) => {
+      console.log({ joinRoomMessages: currentMessages });
+      setMessages(currentMessages);
+    });
+    return () => {
+      socket.off('newMessage');
+    };
+  }, [messages]);
+
+  const sendMessage = (message) => {
+    socket.emit("sendMessage", {
+      room_id: currentRoom.id,
+      User: { username },
+      message,
+    });
+    console.log({ messages });
+    setMessages([...messages, { message, User: { username } }]);
+  };
 
   return (
     <div className="chat d-flex">
       <div className="left d-flex flex-column">
         <Navbar />
-        <Rooms rooms={state.rooms} />
+        <Rooms socket={socket} rooms={rooms} />
       </div>
 
       <Route path="/rooms/:roomId">
-        <Room socket={socket} room={state.currentRoom} />
+        <Room
+          socket={socket}
+          room={currentRoom}
+          username={username}
+          messages={messages}
+          sendMessage={sendMessage}
+        />
       </Route>
     </div>
   );
