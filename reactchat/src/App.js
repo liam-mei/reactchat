7518b1204453975cd7299a189f7f84e807a9f0bd
Derrick Mei <derrick.mei.168@gmail.com>
2020-09-15 00:59:41 -0500

    updated App.js: SocketContest provider implemented
    onLoad, we check for token, if there is token
    a socket is created and page is pushed to /rooms

--- reactchat/src/App.js
+++ reactchat/src/App.js
@@ -1,12 +1,36 @@
-import React, { useEffect, useState } from "react";
+import React, { useState, useEffect } from "react";
+import { useHistory } from "react-router-dom";
 import AppRouter from "./components/AppRouter";
 import "./App.css";
 
+import getToken from "./utils/getToken";
+
+import { SocketContext } from "./contexts/SocketContext";
+import io from "socket.io-client";
+
 function App() {
+  const history = useHistory();
+  const token = getToken();
+  const [socket, setSocket] = useState({
+    emit: () => {},
+    on: () => {},
+    off: () => {},
+  });
+
+  useEffect(() => {
+    if (token) {
+      const newSocket = io.connect("http://localhost:5000");
+      setSocket(newSocket);
+      history.push('/rooms')
+    }
+  }, []);
+
   return (
-    <div className='app'>
-      <AppRouter />
-    </div>
+    <SocketContext.Provider value={{ socket, setSocket }}>
+      <div className="app">
+        <AppRouter />
+      </div>
+    </SocketContext.Provider>
   );
 }
 
