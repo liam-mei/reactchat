d62300291480123c6c3289f5bacfe2d90e9a3953
Derrick Mei <derrick.mei.168@gmail.com>
2020-09-28 03:40:25 -0500

    converted App.js to use socketCreator function

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React, { useState, useEffect } from "react";
 import { useHistory } from "react-router-dom";
 import AppRouter from "./components/AppRouter";
-import secrets from './secrets'
+// import secrets from "./secrets";
 import "./App.css";
 
 import getToken from "./utils/getToken";
+import socketCreator from "./socket/socketCreator";
 
 import { SocketContext } from "./contexts/SocketContext";
-import io from "socket.io-client";
 
 function App() {
   const history = useHistory();
@@ -20,13 +20,9 @@ function App() {
 
   useEffect(() => {
     if (token) {
-      const newSocket = io.connect(secrets.backendURL, {
-        query: { token },
-      });
-      setSocket(newSocket);
-      history.push("/rooms");
+      setSocket(socketCreator(token, history));
     }
-  }, []);
+  }, [token, history]);
 
   return (
     <SocketContext.Provider value={{ socket, setSocket }}>
