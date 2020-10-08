2d22fc637475c8eec8f902c5f88580cc524ee786
Derrick Mei <derrick.mei.168@gmail.com>
2020-10-08 03:22:25 -0500

    updated Rooms.js - added search capability

--- src/components/Rooms.js
+++ src/components/Rooms.js
@@ -1,28 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
 import { useHistory, useLocation } from "react-router-dom";
 
 export default function Rooms(props) {
   const { pathname } = useLocation();
   const history = useHistory();
-  const { rooms, username } = props;
+  const { rooms, username, socket } = props;
   console.log("rooms: ", rooms);
+
+  const [room, setRoom] = useState("");
+  const findRoom = (e) => {
+    setRoom(e.target.value);
+    socket.emit("searchRooms", e.target.value);
+  };
 
   const goToRoom = (e) => {
     console.log(e.target);
-    history.push(`/rooms/${e.target.dataset.id}`);
+    history.push(`/rooms/t/${e.target.dataset.id}`);
     // history.push(`/rooms/1`);
   };
   return (
     <div className="rooms">
+      <input
+        className="searchRooms rounded-pill"
+        type="text"
+        placeholder="Search Rooms"
+        autoComplete="off"
+        spellCheck="false"
+        aria-label="Search Rooms"
+        value={room}
+        onChange={findRoom}
+      />
       {Object.keys(rooms).map((key) => {
         const room = rooms[key];
         const lastMessage = room.messages[room.messages.length - 1] || {
           message: "",
           user: { username: "" },
         };
-        console.log(room.id);
+        // console.log(room.id);
         const lastUser = lastMessage.user.username;
-        console.log(username);
+        // console.log(username);
         const user = lastUser === username ? "you" : lastUser;
         return (
           <div
